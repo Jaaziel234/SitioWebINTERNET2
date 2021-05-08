8c33965d203c1cd72b8a3484bce17156362761be
jose25-pixel <64154119+jose25-pixel@users.noreply.github.com>
2021-05-07 22:14:04 -0600

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,36 @@
 /*Codigo para sitio web*/
+
+$(document).ready(function() {
+
+
+    /*
+
+        //ocultar
+        $("#most").mouseenter(function() {
+            $('#target').show(1000);
+            
+        });
+    //mostrar
+        $("#mostr").mouseleave(function() {
+            $('#tar').hide(3000);
+            $('.target').hide("fast");
+        });*/
+
+
+
+    $("#mostrar").click(function() {
+        $('#target').show(1000);
+
+    });
+    $("#ocultar").click(function() {
+        $('#target').hide(1000);
+
+    });
+    $("#mostrar2").click(function() {
+        $('#target2').show(1000);
+
+    });
+    $("#ocultar2").click(function() {
+        $('#target2').hide(1000);
+
+    });
